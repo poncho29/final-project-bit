6e11daca2555b2e47afeb0abb0664eb75b03ea6d
Poncho29 <meneses0828@gmail.com>
2021-11-29 22:37:37 -0500

    Conexion base de datos clouster

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,7 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //Le pasamos la ruta donde esta la base de datos, si no existe la crea
 
-mongoose.connect('mongodb://localhost/ecommerce')
-.then(db => console.log('Base de datos conectada'))
-.catch(err => console.log(err));
+// mongoose.connect('mongodb://localhost/ecommerce')
+// .then(db => console.log('Base de datos conectada'))
+// .catch(err => console.log(err));
+
+mongoose
+  .connect(
+    "mongodb+srv://adminbit:grupodos@cluster0.ibafa.mongodb.net/ecommerceBit?retryWrites=true&w=majority"
+  )
+  .then((db) => console.log("Base de datos conectada"))
+  .catch((err) => console.log(err));
